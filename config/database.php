b936cbba0dbc0229404c8415659caf44f4abdad4
selkisamet <sametselki@outlook.com>
2026-01-02 18:41:47 +0300

    chore: remove Turkish comment from database config

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'host' => envRequired('DB_HOST'),
     'dbname' => envRequired('DB_NAME'),
     'username' => envRequired('DB_USER'),
-    'password' => env('DB_PASS', ''), // Şifre boş olabilir (localhost için)
+    'password' => env('DB_PASS', ''),
     'charset' => env('DB_CHARSET', 'utf8mb4'),
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
